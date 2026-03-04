c983a7a1f5f1558e6be1ff8c394356ec2133cea7
JackYouTube1 <jackfalconerstc@gmail.com>
2026-03-04 17:07:19 +0200

    new

--- ContactActivity-main/MyContacts/app/src/main/java/com/example/mycontacts/MainActivity.kt
+++ ContactActivity-main/MyContacts/app/src/main/java/com/example/mycontacts/MainActivity.kt
@@ -5,33 +5,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
-        //code here
-        val firstName = "Jack"
-        val lastName = "Falconer"
-        var address = "51 john Rumble Street"
-        var isMale = "true"
-        var dayOfBirth = "20"
-        var monthOfBirth = "06"
-        var yearOfBirth = "2003"
-
-        //Log.d("hello! my name is $firstName $lastName")
-       // Log.d("I live at $address" + "my birthday is $monthOfBirth/$dayOfBirth/$yearOfBirth" + "and I am a ${if (isMale) "male" else "female"}")
-
-        Log.d("ContactInfo", "Hello! My name is $firstName $lastName")
-
-
-
-
-
-
-
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
