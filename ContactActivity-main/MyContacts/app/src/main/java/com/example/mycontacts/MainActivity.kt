9101529388bf2fa66cd6905892648ba5516cb6b3
JackYouTube1 <jackfalconerstc@gmail.com>
2026-03-13 13:56:25 +0200

    Calculate student age in days

--- ContactActivity-main/MyContacts/app/src/main/java/com/example/mycontacts/MainActivity.kt
+++ ContactActivity-main/MyContacts/app/src/main/java/com/example/mycontacts/MainActivity.kt
@@ -27,7 +27,49 @@ class MainActivity : AppCompatActivity() {
 
 
 
+      //Calculate wages
+        val rateOfPay = 20
+        val hoursWorked = 50
+        val overTime = 1.5
 
+        if (hoursWorked > 40) {
+
+            val regularPay = 40 * rateOfPay
+            val overTimeHours = hoursWorked - 40
+            val overtimePay = overTimeHours * rateOfPay * overTime
+
+            val totalPay = regularPay + overtimePay
+
+            println("Overtime hours: $overTimeHours")
+            println("Total pay: $totalPay")
+
+        } else {
+
+            val regularPay = hoursWorked * rateOfPay
+            println("Regular hours pay: $regularPay")
+        }
+
+        // Age calculations
+        val currentYear = 2024
+        val ageInYears = currentYear - yearOfBirth.toInt()
+
+        // Years until the student turns 100
+        val yearsUntil100 = (100 - ageInYears)
+
+        // Age in months
+        val ageInMonths = ageInYears * 12
+
+        // Age in days (skipping leap years)
+        val ageInDays = ageInYears * 365
+
+        // Remainder when age is divided by 5
+        val ageRemainderBy5 = ageInYears % 5
+
+        // Print all calculated values
+        Log.d("AgeCalculations", "Years until turning 100: $yearsUntil100")
+        Log.d("AgeCalculations", "Age in months: $ageInMonths")
+        Log.d("AgeCalculations", "Age in days (no leap years): $ageInDays")
+        Log.d("AgeCalculations", "Age remainder divided by 5: $ageRemainderBy5")
 
 
 
